bdc9125858794fc1d13a8421361b674d02ce288b
Florent Beaurain <beaurain.florent@gmail.com>
2021-05-07 23:29:24 +0200

    Add Rails integration (#3)

--- test/stagery_test.rb
+++ test/stagery_test.rb
@@ -22,10 +22,34 @@ class StageryTest < Minitest::Test
     assert_equal "development", Stagery.stage
   end
 
+  def test_fallback_to_rails_env_if_stagery_stage_is_blank_and_rails_defined
+    ENV['STAGERY_STAGE'] = nil
+
+    with_rails_env_production do
+      assert_equal "production", Stagery.stage
+    end
+  end
+
   def test_stage_can_be_set_at_runetime
     ENV['STAGERY_STAGE'] = 'staging'
     Stagery.stage = 'production'
 
     assert_equal "production", Stagery.stage
+  end
+
+  private
+
+  def with_rails_env_production
+    Object.const_set("Rails", eval(<<~RUBY))
+      Module.new do
+        def self.env
+          "production"
+        end
+      end
+    RUBY
+
+    yield
+  ensure
+    Object.send(:remove_const, "Rails")
   end
 end
